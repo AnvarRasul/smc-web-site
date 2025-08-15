29a943e89b0176d6314d9756b1eb3f80c7697ff7
AnvarRasul <anvarjon.rasulov777@gmail.com>
2025-08-15 07:39:48 -0200

    organizationsSection

--- src/components/layouts/OrganizationsSection/OrganizationsSection.jsx
+++ src/components/layouts/OrganizationsSection/OrganizationsSection.jsx
@@ -11,11 +11,62 @@ function Organizations() {
                 <h2>ДЛЯ КОГО ЭТО РЕШЕНИЕ</h2>
             </div>
             <div className={styles.organizations_content}>
-                <div className={styles.item-top_left}>
+                <div className={styles.left_content}>
+                <div className={`${styles.item} ${styles["top-left"]}`}>
+                    <div className={styles.box}>
+                        <h3>ЭКОЛОГИЧЕСКИЕ ОРГАНИЗАЦИИ</h3>
+                        <p>Для контроля и мониторинга экологических рисков</p>
+                    </div>
+                    <span className="line"></span>
+                </div>
 
+                <div className={`${styles.item} ${styles["mid-left"]}`}>
+                    <div className={styles.box}>
+                        <h3>АГРАРНЫЙ СЕКТОР</h3>
+                        <p>Для сельхозкомпаний, заинтересованных в анализе посевов</p>
+                    </div>
+                    <span className="line"></span>
+                </div>
+
+                <div className={`${styles.item} ${styles["bottom-left"]}`}>
+                    <div className={styles.box}>
+                        <h3>КАДАСТРОВЫЕ СЛУЖБЫ</h3>
+                        <p>Для точного определения и учёта земельных участков</p>
+                    </div>
+                    <span className="line"></span>
                 </div>
             </div>
-            
+
+            <div className={styles.globe}>
+                <img src={globe} alt="Globe"/>
+            </div>
+
+                <div className={styles.rigth_content}>
+                <div className={`${styles.item} ${styles["top-rigth"]}`}>
+                    <div className={styles.box}>
+                        <h3>ГОССТРУКТУРЫ</h3>
+                        <p> Для органов власти, которым необходим оперативный контроль</p>
+                    </div>
+                    <span className="line"></span>
+                </div>
+
+                <div className={`${styles.item} ${styles["mid-rigth"]}`}>
+                    <div className={styles.box}>
+                        <h3>НАУЧНЫЕ ИНСТИТУТЫ</h3>
+                        <p>Для проведения исследований с использованием спутниковых данных</p>
+                    </div>
+                    <span className="line"></span>
+                </div>
+
+                <div className={`${styles.item} ${styles["bottom-rigth"]}`}>
+                    <div className={styles.box}>
+                        <h3>ИНВЕСТИЦИОННЫЕ КОМПАНИИ</h3>
+                        <p>Для оценки рисков и планирования инвестиций</p>
+                    </div>
+                    <span className="line"></span>
+                </div>
+                </div>
+            </div>
         </section>
     );
 }
